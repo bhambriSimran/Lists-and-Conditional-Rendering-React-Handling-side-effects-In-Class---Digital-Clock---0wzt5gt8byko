4e0a466dc9c667ae3e8f1ee18a13e73505afe58b
Simran Bhambri <93468452+bhambriSimran@users.noreply.github.com>
2021-12-31 12:55:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -84,37 +84,37 @@
 
 
 
-import "../styles/App.css";
-import React, { Component } from "react";
+// import "../styles/App.css";
+// import React, { Component } from "react";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      time: new Date().toLocaleTimeString(),
-    };
-  }
-  update() {
-    this.setState({ time: new Date().toLocaleTimeString() });
-  }
-  componentDidMount() {
-    this.timer = setInterval(() => {
-      this.update();
-    }, 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
-  render() {
-    return (
-      <div className="Clock">
-        <h3 id="time">{this.state.time}</h3>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       time: new Date().toLocaleTimeString(),
+//     };
+//   }
+//   update() {
+//     this.setState({ time: new Date().toLocaleTimeString() });
+//   }
+//   componentDidMount() {
+//     this.timer = setInterval(() => {
+//       this.update();
+//     }, 1000);
+//   }
+//   componentWillUnmount() {
+//     clearInterval(this.timer);
+//   }
+//   render() {
+//     return (
+//       <div className="Clock">
+//         <h3 id="time">{this.state.time}</h3>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
 
 // const App = () => {
 //   let [time, setTime] = useState(new Date().toLocaleTimeString());
@@ -138,4 +138,30 @@ export default App;
 // };
 
 // export default App;
+
+////////////////////////////////////////////////
+import React, { useState } from 'react'
+import '../styles/App.css';
+const App = () => {
+  const dateTime = new Date().toLocaleString();
+
+  const[currentTime,setCurrentTime] = useState(dateTime);
+
+  const update = () => {
+    const dateTime = new Date().toLocaleString();
+    setCurrentTime(dateTime)
+  }
+
+  setInterval(update,1000);
+  return (
+    <div id="main">
+      <div className="date-time">
+        <h1>{currentTime}</h1>
+      </div>
+    </div>
+  )
+}
+
+
+export default App;
 
