bdcbd6d214c5ffdbb5995baf7383c70023a51aa9
Simran Bhambri <93468452+bhambriSimran@users.noreply.github.com>
2021-12-31 12:48:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,89 @@
-// import React from 'react'
-// import '../styles/App.css';
-// const App = () => {
+// // import React from 'react'
+// // import '../styles/App.css';
+// // const App = () => {
 
-//   return (
-//     <div id="main">
-//       <div className="date-time"></div>
-//     </div>
-//   )
+// //   return (
+// //     <div id="main">
+// //       <div className="date-time"></div>
+// //     </div>
+// //   )
+// // }
+
+
+// // export default App;
+// import "../styles/App.css";
+// import React, { Component } from "react";
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       time: new Date().toLocaleTimeString(),
+//     };
+//   }
+//   update() {
+//     this.setState({ time: new Date().toLocaleTimeString() });
+//   }
+//   componentDidMount() {
+//     this.timer = setInterval(() => {
+//       this.update();
+//     }, 1000);
+//   }
+//   componentWillUnmount() {
+//     clearInterval(this.timer);
+//   }
+//   render() {
+//     return (
+//       <div className="Clock">
+//         <h3 id="time">{this.state.time}</h3>
+//       </div>
+//     );
+//   }
 // }
 
-
 // export default App;
+
+// // const App = () => {
+// //   let [time, setTime] = useState(new Date().toLocaleTimeString());
+// //   const updateTime = () => {
+// //     let currTime = new Date().toLocaleTimeString();
+// //     setTime(currTime);
+// //   };
+// //   useEffect(() => {
+// //     let timer = setInterval(()=>{
+// //       updateTime();
+// //     }, 1000);
+// //     return () => {
+// //       clearInterval(timer);
+// //     };
+// //   }, [time]);
+// //   return (
+// //     <div className="Clock">
+// //       <h3 id="time">{time}</h3>
+// //     </div>
+// //   );
+// // };
+
+// // export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import "../styles/App.css";
 import React, { Component } from "react";
 
@@ -65,3 +138,4 @@ export default App;
 // };
 
 // export default App;
+
